43e2717b91c2fa1038cb2c0417bc77f0d790e6b3
KasiaHajduk <kasiahajduk2014@gmail.com>
2021-11-30 12:31:11 +0100

    oo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+let throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+player.getVideoTitle().then(function (title) {
+    console.log('title: ', title);
+});
+
+
+
+player.on('timeupdate', throttle(event => {
+    console.log(event.seconds);
+    localStorage.setItem('videoplayer-current-time', event.seconds)
+}, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+let throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+let email = document.querySelector('input');
+let message = document.querySelector('textarea');
+
+console.log(form);
+
+let followe = {};
+let storage = {};
+
+// const following = throttle(event => {
+//     console.log('followingprzed' + localStorage.getItem('feedback-form-state'));
+
+//     //console.log(event.target);
+//     console.log(event.target.name);
+//     console.log(event.target.value);
+//     follow[event.target.name] = event.target.value;
+//     localStorage.setItem('feedback-form-state', JSON.stringify(follow));
+//     console.log('followingpooooo' + localStorage.getItem('feedback-form-state'));
+// }, 500
+// );
+
+const following = throttle(event => {
+  console.log('przed' +localStorage.getItem('feedback-form-state'));
+    console.log(event.target.name);
+  console.log(event.target.value);
+      console.log(event.target.attributes[0].textContent);
+
+//  storage[event.target.attributes[0].textContent] = event.target.value;
+  storage[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(storage));
+  console.log('pooo' +localStorage.getItem('feedback-form-state'));
+}, 500);
+
+
+
+email.addEventListener('input', following);
+message.addEventListener('input', following);
+
+
+
+// form.addEventListener('submit', sendMessage);
+// function sendMessage(event) {
+//     event.preventDefault();
+//     console.log(localStorage.getItem('feedback-form-state'));
+//     updateOutput();
+//     //form.reset();
+// }
+
+updateOutput();
+function updateOutput() {
+    console.log('lS' + localStorage.getItem('feedback-form-state'));
+    let update = JSON.parse(localStorage.getItem('feedback-form-state'));
+    email.value = update.email || "";
+    message.value = update.message || "";
+        console.log('lSpo' + localStorage.getItem('feedback-form-state'));
+
+  };
