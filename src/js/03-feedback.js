e6de6185df99ee0927873eb4b531da4c5b0b1481
KasiaHajduk <kasiahajduk2014@gmail.com>
2021-12-02 18:12:32 +0100

    ttt

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,60 +1,40 @@
 let throttle = require('lodash.throttle');
 
-const form = document.querySelector('.feedback-form');
+let form = document.querySelector('form');
 let email = document.querySelector('input');
 let message = document.querySelector('textarea');
+let btn = document.querySelector('button');
 
-console.log(form);
 
-let followe = {};
-let storage = {};
-
-// const following = throttle(event => {
-//     console.log('followingprzed' + localStorage.getItem('feedback-form-state'));
-
-//     //console.log(event.target);
-//     console.log(event.target.name);
-//     console.log(event.target.value);
-//     follow[event.target.name] = event.target.value;
-//     localStorage.setItem('feedback-form-state', JSON.stringify(follow));
-//     console.log('followingpooooo' + localStorage.getItem('feedback-form-state'));
-// }, 500
-// );
+let follow = {};
 
 const following = throttle(event => {
-  console.log('przed' +localStorage.getItem('feedback-form-state'));
-    console.log(event.target.name);
-  console.log(event.target.value);
-      console.log(event.target.attributes[0].textContent);
-
-//  storage[event.target.attributes[0].textContent] = event.target.value;
-  storage[event.target.name] = event.target.value;
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(storage));
-  console.log('pooo' +localStorage.getItem('feedback-form-state'));
+  follow[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(follow));
 }, 500);
 
 
+btn.addEventListener('click', event => {
+  event.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.clear();
+  updateForm();
+  form.reset();
+  follow = {};
+});
 
+
+const updateForm = () => {
+  let update = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (update) {
+    follow = update;
+    email.value = follow.email;
+    message.value = follow.message;
+  }
+  else return;
+};
+  
+updateForm();
 email.addEventListener('input', following);
 message.addEventListener('input', following);
 
-
-
-// form.addEventListener('submit', sendMessage);
-// function sendMessage(event) {
-//     event.preventDefault();
-//     console.log(localStorage.getItem('feedback-form-state'));
-//     updateOutput();
-//     //form.reset();
-// }
-
-updateOutput();
-function updateOutput() {
-    console.log('lS' + localStorage.getItem('feedback-form-state'));
-    let update = JSON.parse(localStorage.getItem('feedback-form-state'));
-    email.value = update.email || "";
-    message.value = update.message || "";
-        console.log('lSpo' + localStorage.getItem('feedback-form-state'));
-
-  };
